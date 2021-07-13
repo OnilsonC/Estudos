7e2d8367211079db6d68a43e0fcea29d01783021
unknown <onilldta@yahoo.com.br>
2021-07-13 17:43:04 -0300

    classe SAPCommerce

---
+++ SAPCommerce.java
@@ -0,0 +1,12 @@
+public class SAPCommerce {
+
+	public void addToCart() {
+		System.out.println("Adicionar item ao carrinho");
+ 	}
+
+ 	public void removeItemFromCart() {
+ 		System.out.println("Remover item do carrinho");
+ 	}
+
+
+}
