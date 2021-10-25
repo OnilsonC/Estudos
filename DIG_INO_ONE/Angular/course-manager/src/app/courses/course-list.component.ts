d431984d60db03c94c522f6b06d5834c7fc816e2
unknown <onilldta@yahoo.com.br>
2021-10-25 18:56:30 -0300

    Introdução ao Angular

--- DIG_INO_ONE/Angular/course-manager/src/app/courses/course-list.component.ts
+++ DIG_INO_ONE/Angular/course-manager/src/app/courses/course-list.component.ts
@@ -1,38 +1,43 @@
-import { style } from "@angular/animations";
+import { CourseService } from './course.service';
 import { Component, OnInit } from "@angular/core";
 import { Course } from "./course";
 
 @Component ({
-    selector: 'app-course-list',
     templateUrl: './course-list.component.html',
-    styleUrls: ['./course-list.component.css']
-})
+    })
 export class CourseListComponent implements OnInit{
 
-  courses: Course[] = [];
+  filterCourse: Course[] = [];
+
+  _courses: Course[] = [];
+
+  _filterBy!: string;
+
+  constructor (private courseService: CourseService) {}
 
   ngOnInit(): void {
-    this.courses = [
-        {
-        id: 1,
-        name: 'Angular: Forms',
-        imageUrl: '/assets/images/forms.png',
-        price: 100.00,
-        code: 'AFO01',
-        duration: 80,
-        rating: 4,
-        releaseDate: 'Octuber, 7, 2021'
-      },
-      {
-        id: 2,
-        name: 'Angular: HTML',
-        imageUrl: '/assets/images/http.png',
-        price: 130.00,
-        code: 'AHTMLO01',
-        duration: 100,
-        rating: 5,
-        releaseDate: 'November, 3, 2021'
-     }
-   ]
+    this.retrieveAll();
   }
+
+  retrieveAll(): void {
+    this.courseService.retriveAll().subscribe({
+      next: courses => {
+        this._courses = courses;
+        this.filterCourse = this._courses;
+      },
+      error: err => console.log('Error', err)
+    })
+  }
+
+  set filter(value: string){
+    this._filterBy = value;
+
+    this.filterCourse = this._courses.filter((course: Course) =>
+    course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase())> -1)
+  }
+
+  get filter() {
+    return this._filterBy;
+  }
+
 }
